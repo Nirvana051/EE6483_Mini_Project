16f55014ca13d17684d06dce0c231c18dbc52546
Zhongyuna.Liu <zliu051@e.ntu.edu.sg>
2024-11-14 19:30:56 +0800

    Update data_processing.py

--- data_processing.py
+++ data_processing.py
@@ -3,42 +3,42 @@ import random
 from tqdm import tqdm
 import shutil
 
-
 if __name__=="__main__":
-
-    root="./data"   #数据文件根目录
-    trainPath="./datasets/train"  #训练集根目录
-    valPath="./datasets/val"  #验证集根目录
-    train_num=20000  #训练集数目
-    val_num=5000
+    root="./data"   # Root data directory
+    trainPath="./datasets/train"  # Training set directory
+    valPath="./datasets/val"  # Validation set directory
+    train_num=20000  # Number of training samples
+    val_num=5000     # Number of validation samples
     classes=["cat","dog"]
-
-    for folder in os.listdir(root):   #一开始先删除里面原有的照片
+    
+    # Initially delete existing images
+    for folder in os.listdir(root):
         folderpath=os.path.join(root,folder)
         if os.path.isdir(folderpath):
             for pic in os.listdir(folderpath):
                 if pic.endswith(".jpg"):
                     file=os.path.join(folderpath,pic)
                     os.remove(file)
-        print("删除成功")
-
+        print("Deletion successful")
+    
+    # Copy training images
     for cls in classes:
         folder_path=os.path.join(trainPath,cls)
         pictures=os.listdir(folder_path)
-        random.shuffle(pictures)  #打乱顺序
+        random.shuffle(pictures)  # Shuffle order
         for index in tqdm(range(int(train_num/len(classes)))):
-            #复制到训练集 
+            # Copy to training set 
             oldpath=os.path.join(folder_path,pictures[index])
             newpath=os.path.join(root,"train",str(index)+"_"+cls.lower()+".jpg")
             shutil.copy(oldpath,newpath)
     
+    # Copy validation images
     for cls in classes:
         folder_path=os.path.join(valPath,cls)
         pictures=os.listdir(folder_path)
-        random.shuffle(pictures)  #打乱顺序
+        random.shuffle(pictures)  # Shuffle order
         for index in tqdm(range(int(val_num/len(classes)))):
-            #复制到验证集 
+            # Copy to validation set 
             oldpath=os.path.join(folder_path,pictures[index])
             newpath=os.path.join(root,"val",str(index)+"_"+cls.lower()+".jpg")
             shutil.copy(oldpath,newpath)
-        
