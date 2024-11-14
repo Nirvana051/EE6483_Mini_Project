a57c8d1c54f759ed76ffad8c56984f5744eea6b2
Zhongyuna.Liu <zliu051@e.ntu.edu.sg>
2024-11-14 19:32:45 +0800

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,13 +1,12 @@
-
 import os
 import torch
 from torchvision import transforms
 from PIL import Image
-# import matplotlib.pyplot as plt
-import models
 import csv
 
-def predict(root,imgname, model, img_trans):
+import models
+
+def predict(root, imgname, model, img_trans):
     imgpath = os.path.join(root, imgname)
     img_rgb = Image.open(imgpath).convert("RGB")
     img_tensor = img_trans(img_rgb).unsqueeze(0)
@@ -22,62 +21,49 @@ def predict(root,imgname, model, img_trans):
         pred_prob, pred_idx = torch.max(prob, dim=1)
         pred_prob = pred_prob.item()
         pred_idx = pred_idx.item()
-
-    if pred_idx == 0:
-        pred_str = "cat"
-    else:
-        pred_str = "dog"
+    
+    pred_str = "cat" if pred_idx == 0 else "dog"
     id = imgname.split("_")[0]
-    label = imgname.split("_")[1]
-    label = label.split(".")[0]
+    label = imgname.split("_")[1].split(".")[0]
     filepath = "./output/submission.csv"
     csv_writer(filepath, id, label, pred_str)
     return label, pred_str
 
-    # plt.imshow(img_rgb)
-    # plt.title("Predicted: {} ,Probability: {:.2f}".format(pred_str, pred_prob))
-    # plt.savefig("output/pre_" + imgname)
-
 def csv_writer(filepath, id, label, pr):
-    with open(filepath,"a", newline="") as csvfile: 
+    with open(filepath, "a", newline="") as csvfile: 
         writer = csv.writer(csvfile)
-        #写入多行用writerows
-        writer.writerow([id,label,pr])
-
+        writer.writerow([id, label, pr])
 
 if __name__ == "__main__":
-
-    # 图像预处理
+    # Image preprocessing
     img_trans = transforms.Compose([
         transforms.CenterCrop(224),
         transforms.Resize([224, 224]),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-
-    model = models.ResNet34() # 模型结构
-    # modelpath = "./runs/SqueezeNet_ep=100_bs=32_lr=5e-05_ld=0.9_wd=0/SqueezeNet_best.pth"
-    modelpath = "./runs/ResNet34_ep=100_bs=32_lr=5e-05_ld=0.9_wd=0/ResNet34_best.pth" # 训练好的模型路径
-    checkpoint = torch.load(modelpath)  
-    model.load_state_dict(checkpoint)  # 加载模型参数
     
-    with open("./output/submission.csv","w", newline="") as csvfile: 
+    model = models.ResNet34() # Model structure
+    modelpath = "./runs/ResNet34_ep=100_bs=32_lr=5e-05_ld=0.9_wd=0/ResNet34_best.pth" # Trained model path
+    checkpoint = torch.load(modelpath)  
+    model.load_state_dict(checkpoint)  # Load model parameters
+    
+    with open("./output/submission.csv", "w", newline="") as csvfile: 
         writer = csv.writer(csvfile)
-        #先写入columns_name
-        writer.writerow(["id","label","predict result"])
-
+        writer.writerow(["id", "label", "predict result"])
+    
     root = "./datasets/test"
-
+    
     all = 0
     correct = 0
-
+    
     pathlist = os.listdir(root)
-    pathlist = sorted(pathlist,key=lambda x: int(x.split("_")[0]))
-
+    pathlist = sorted(pathlist, key=lambda x: int(x.split("_")[0]))
+    
     for pic in pathlist:
-        all = all + 1
+        all += 1
         if pic.endswith(".jpg"):
-            label,pre = predict(root,pic, model, img_trans)
+            label, pre = predict(root, pic, model, img_trans)
             if label == pre:
-                correct = correct + 1
-    print("acc = " + str(correct/all) + " all = " + str(all) + " correct = " + str(correct))
+                correct += 1
+    print(f"Accuracy = {correct/all}, Total = {all}, Correct = {correct}")
